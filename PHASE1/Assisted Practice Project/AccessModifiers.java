1d4c0b701992f5c2aff6c7504eba0d8f06379b6a
kovijayanth24 <99873311+kovijayanth24@users.noreply.github.com>
2022-02-21 09:52:07 +0530

    Create AccessModifiers.java

---
+++ PHASE1/Assisted Practice Project/AccessModifiers.java
@@ -0,0 +1,13 @@
+package Jayanth;
+
+public class AccessModifiers {
+	    void display()
+	    {
+	        System.out.println("Jayanth");
+	    }
+	    public static void main(String[] args) {
+	        AccessModifiers obj = new AccessModifiers();
+	        obj.display();
+	    }
+	}
+
