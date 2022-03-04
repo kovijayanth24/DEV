f0d32cd83e697f756fde03916c2cd00957ecbfc6
kovijayanth24 <99873311+kovijayanth24@users.noreply.github.com>
2022-03-04 11:52:47 +0530

    Create Regularexp.java

---
+++ PHASE1/Assisted Practice Project/Regularexp.java
@@ -0,0 +1,16 @@
+package Jayanth;
+	import java.util.regex.*;
+
+	public class Regularexp {
+
+	    public static void main(String[] args) {
+
+	        String pattern = "[a-z]+";
+	        String check = "Regular Expressions";
+	        Pattern p = Pattern.compile(pattern);
+	        Matcher c = p.matcher(check);
+
+	        while (c.find())
+	            System.out.println( check.substring( c.start(), c.end() ) );
+	    }
+}
