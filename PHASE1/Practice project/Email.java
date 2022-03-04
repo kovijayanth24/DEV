53c3958e7c3bd7167cfd630fb2f6ba0b6ada80bc
kovijayanth24 <99873311+kovijayanth24@users.noreply.github.com>
2022-03-04 20:11:38 +0530

    Create Email.java

---
+++ PHASE1/Practice project/Email.java
@@ -0,0 +1,39 @@
+package Jayanth;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Email {
+
+	   public static boolean isValidEmail(String email) {
+		   String regex = "^(.+)@(.+)$";
+		 //initialize the Pattern object
+	       Pattern pattern = Pattern.compile(regex);
+	       Matcher matcher = pattern.matcher(email);
+	       return matcher.matches();
+
+	       
+	   }
+
+	   public static void main(String[] args) {
+	       List<String> emails = new ArrayList<String>();
+	       // valid email addresses
+	       emails.add("Pradeep@example.com");
+	       emails.add("Reshmitha@example.com");
+	       emails.add("Minie@example.me.org");
+	       emails.add("Pradeepa.example.com");
+	       emails.add("Hari.bob@example.com");
+	       emails.add("Pradheep@.example.com");
+
+	       for (String value : emails) {
+	           System.out.println("The Email address " + value + " is " + (isValidEmail(value) ? "valid" : "invalid"));
+	       }
+	       
+	       System.out.println("Enter any email address to check");
+	       Scanner sc = new Scanner(System.in);
+	       String input = sc.nextLine();
+	       System.out.println("The Email address " + input + " is " + (isValidEmail(input) ? "valid" : "invalid"));
+	       }
+}
