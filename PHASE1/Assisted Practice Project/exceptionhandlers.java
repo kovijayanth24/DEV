3c2ff1912b9011d4a64a151ead70f005919722c1
kovijayanth24 <99873311+kovijayanth24@users.noreply.github.com>
2022-03-04 20:14:24 +0530

    Create exceptionhandlers.java

---
+++ PHASE1/Assisted Practice Project/exceptionhandlers.java
@@ -0,0 +1,23 @@
+package Jayanth;
+class exceptionhandlers extends Exception{
+   String str1;
+   exceptionhandlers(String str2) {
+	str1=str2;
+   }
+   public String toString(){ 
+	return ("exceptionhandlers Occurred: "+str1) ;
+   }
+}
+class Example1{
+   public static void main(String args[]){
+	try{
+		System.out.println("Starting of try block");
+		// I'm throwing the custom exception using throw
+		throw new exceptionhandlers("This is My error Message");
+	}
+	catch(exceptionhandlers exp){
+		System.out.println("Catch Block") ;
+		System.out.println(exp) ;
+	}
+   }
+}
